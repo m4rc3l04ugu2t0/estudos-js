33c17a830ac61ddec3184a36b2d23ece23300bc1
m4rc3l04ugu2t0 <mrcelo014@gmail.com>
2023-12-14 18:55:59 -0300

    trabalhando com diretorios

--- curso-js/node+express/A.124-147/a-130/server.js
+++ curso-js/node+express/A.124-147/a-130/server.js
@@ -5,4 +5,36 @@ app.get('/', (request, response) => {
   response.send('hello word');
 });
 
-app.listen(301);
+app.get('/contact', (request, response) => {
+  response.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Formulário</title>
+</head>
+<body>
+
+    <form action="/" method="post">
+        <label for="campo">Campo:</label>
+        <input type="text" id="campo" name="campo" required>
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+
+</body>
+</html>
+`);
+});
+
+app.listen(3001, () => {
+  console.log('running on port 3001,  http://localhost:3001');
+});
+
+app.listen(3002, () => {
+  console.log('running contact, port 3002');
+});
+
+app.post('/', (request, response) => {
+  response.send('form received');
+});
