479de11c233fbb959d660e777a99f4200a015784
m4c3l04ugu2t0 <mrcelo014@gmail.com>
2023-12-21 16:54:30 -0300

    aula 136

---
+++ curso-js/node+express/A.124-147/a-136/frontEnd/main.js
@@ -0,0 +1,7 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+alert('bb');
+
+document.write('slaaaaaaa');
+
+console.log('snksnkn');
--- curso-js/node+express/A.124-147/a-136/server.js
+++ curso-js/node+express/A.124-147/a-136/server.js
@@ -4,6 +4,9 @@ const path = require('path');
 const routes = require('./router');
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
